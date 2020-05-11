0c6b9029c833bf6d56c8cb42b8a5dcb7ed86330b
mahir29 <mahirhardwar@gmail.com>
2020-05-11 14:50:53 +0530

    passport facebook token

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports={
     'secretKey':'12345-67890-09876-54321',
-    'mongoUrl':'mongodb://localhost:27017/conFusion'
+    'mongoUrl':'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId:'563332947934194',
+        clientSecret:'701f8d6482e48ec7f6a46309ef7537b2'
+    }
 }
